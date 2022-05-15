8c2589818cf8c778bb4d4a4f88a0c3fb65d655fc
arunlalpp <arunlalpp@gmail.com>
2022-05-15 21:12:05 +0530

    Style

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { SliderData } from './SliderData';
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
   const ImageSlider = ({slides}) => {
+    
     const [current, setCurrent] = useState(0)
     const length = slides.length
 
